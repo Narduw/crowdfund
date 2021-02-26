df5f87b15a5febfce896cfdeae62ff7fbec1e065
narduw <nunes.bernardo.237@gmail.com>
2021-02-26 08:33:36 +0100

    Hero + Fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import './App.css'
+import Hero from './components/Hero'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="crowdfund-main">
+      <Hero/>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+    return <div className='hero'>
+        <div className='hero-content'>
+            <div className='hero-title'>crowdfund</div>
+            <div className='menu'></div>
+        </div>
+    </div>
+}
